51a929ed45730965e9f96ddf5dbe8114895be4a7
Iniquit <iniquitwrites@gmail.com>
2019-04-22 23:48:19 -0400

    Use mc-ping-updated instead of MCAPI
    + Updated to use mc-ping-updated module instead of MCAPI
    + Added simple command handling from "The Perfect Lil' Bot"
    + Added 'status' command that gives a list of online players
    + Added 'help' command that lists available commands
    + Added 'crash' command that restarts the bot
    + Added console highlighting

--- mcbot.js
+++ mcbot.js
@@ -1,70 +1,138 @@
-//credit to vegeta897 for the request URL part from his 'Simple Minecraft server status bot'
-const Discord = require("discord.js");
+const Discord = require("discord.js"); //Require Discord.js -- a package that allows us to interact with Discord
+mcping = require('mc-ping-updated'); //Require mc-ping-updated -- a package that allows us to ping a Minecraft server
+const chalk = require('chalk'); //Require chalk -- a package that allows us to style console logs with color
+var escape = require('markdown-escape') // Require markdown-escape -- a package that allows us to escape markdown formatting (preventing things like tildes in playernames from showing up in discord as strikethroughs)
 const client = new Discord.Client();
-const settings = require('./config.json');
-var statustring = "No signal";
-
-var request = require('request');
-var mcCommand = '/minecraft'; // Command for triggering
-var mcIP = settings.ip; // Your MC server IP
-var mcPort = settings.port; // Your MC server port
-
-var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+const settings = require('./config.json'); //Location of config file
+const prefix = "/"; //Command prefix
+let onlinePlayers = [];
 
 
-function update() {
-  /*seconds = seconds + 1;
-  secondsString = seconds.toString();
-  client.user.setActivity(secondsString, { type: 'Playing' })
-  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-  .catch(console.error);*/
-  request(url, function(err, response, body) {
-      if(err) {
-          console.log(err);
-          //return message.reply('Error getting Minecraft server status...');
-      }
-      body = JSON.parse(body);
-      var status = 'Server offline';
-      console.log(body.motd);
-      if(body.online) {
-          if((body.motd=="&cWe are under maintenance.")||(body.players.now>=body.players.max)){
-            client.user.setStatus('idle')
-            //.then(console.log)
-            .catch(console.error);
-          }else{
-            client.user.setStatus('online')
-            //.then(console.log)
-            .catch(console.error);
-          }
-            if(body.players.now) {
-                status = ' ' + body.players.now + '  of  ' + body.players.max;
-              } else {
-                status = ' 0  of  ' + body.players.max;
-        }
-      } else {
-        client.user.setStatus('dnd')
-        //.then(console.log)
-        .catch(console.error);
-
-      }
-      client.user.setActivity(status, { type: 'PLAYING' })
-      .then(presence => console.log(status))
+function newUpdate () {
+mcping(settings.ip, settings.port, function(err, res) {
+  if (!(typeof err === 'undefined' || err === null)) {
+        client.user.setStatus('dnd');
+        status2 = 'Server offline';
+        client.user.setActivity(status2, { type: 'PLAYING' });
+      date = new Date();
+cleanDate = date.toLocaleTimeString();
+      console.log((chalk.yellow('\[' + cleanDate + '\]:') + chalk.white(' Ping: ' + 'Error getting server status')));
+        console.error(err); return;}
+    else {
+        if (typeof res.players.sample == 'undefined')
+            {status2 = '0 / 25';
+        client.user.setStatus('idle')
+    date = new Date();
+cleanDate = date.toLocaleTimeString();
+client.user.setActivity(status2, { type: 'PLAYING' })
+      .then(presence => console.log(chalk.cyan('\[' + cleanDate + '\]:') + chalk.white(' Ping: ' + status2)))
+      .catch(console.error); return;}
+        else {
+         status2 = res.players.online + ' / ' + res.players.max
+client.user.setStatus('online');
+date = new Date();
+cleanDate = date.toLocaleTimeString();
+client.user.setActivity(status2, { type: 'PLAYING' })
+      .then(presence => console.log(chalk.cyan('\[' + cleanDate + '\]:') + chalk.white(' Ping: ' + status2)))
       .catch(console.error);
-  });
-
+}}});
 }
+
+
 client.on("ready", () => {
   console.log("I am ready!");
-  client.setInterval(update,30000);
+  client.setInterval(newUpdate,30000);
 });
 
-/*client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-    update();
 
-  }
-}
-);*/
+
+
+
+
+
+//Command Handling
+client.on('message', message => {
+
+    if (!message.content.startsWith(prefix)) return; //If the message doesn't start with our prefix, ignore it
+    if (message.author.bot) return;{ //If the author of the message is a bot (including us)-- ignore it
+
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+//log the above
+console.log('Command \'' + message.content + '\' issued by ' + message.member.user.tag);
+console.log('Args: ' + args);
+
+
+//Help command handling
+if (command === "help" || command === "commands" || command === "list" | command === "bot"){
+
+      console.log('Issuing help message.');
+         message.channel.send({embed: {
+          color: 3447003,
+          title: 'Commands:',
+          description: '**/status** - The current status and playercount of your server \n**/crash** - Restart the bot'
+                            }}); return;
+
+       }
+
+     
+
+
+//Status command handling
+        else if (command === "status" || command === "server"){
+
+mcping('cataclysm.us', 25565, function(err, res) {
+    if (err) {
+        // Some kind of error
+        console.error(err);
+        message.channel.send('Error getting server status.'); return
+    } else {
+      
+        let onlinePlayers = [];
+
+
+if (typeof res.players.sample == 'undefined'){
+    status2 = '*No one is playing!*';
+} else {
+
+          for (var i = 0; i < res.players.sample.length; i++) {
+  
+  onlinePlayers.push(res.players.sample[i].name);
+};
+
+
+
+onlinePlayers = onlinePlayers.sort();
+onlinePlayers = onlinePlayers.join(', ');
+onlinePlayers = escape(onlinePlayers);
+         status2 = '**' + res.players.online + '/' + res.players.max + '**' + ' player(s) online.\n' + onlinePlayers;
+
+console.log('  ' + status2);
+        
+  };
+
+        message.channel.send({embed: {
+                                title: 'Status:',
+                                color: 3447003,
+                                description: status2,
+                                fields: [
+                                  {
+                                    name: "Server Version:",
+                                    value: (res.version.name)
+                                  },
+                                ],
+                              
+                              }
+                            }); 
+    }
+}, 3000);
+return;
+        }
+
+    
+      else if (command === "crash"){process.exit(0);}
+      else return;
+}})
 
 client.login(settings.token);
