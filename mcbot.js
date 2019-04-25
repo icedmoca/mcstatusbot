d79cf83eaf7a5f8be65da3a656ed8791fe74a9f4
Iniquit <levi@leviswartz.net>
2019-04-25 10:21:58 -0400

    Renamed variables more sensibly. cleaned up indentation

--- mcbot.js
+++ mcbot.js
@@ -3,148 +3,113 @@ const mcping = require('mc-ping-updated')
 const chalk = require('chalk')
 const escape = require('markdown-escape')
 const client = new Discord.Client()
-const settings = require('./config.json'); //Location of config file
-
+const settings = require('./config.json');
 var hasIcon = 'n/a'
 
-function getDate(){
-  date = new Date();
-cleanDate = date.toLocaleTimeString();
+function getDate() {
+    date = new Date();
+    cleanDate = date.toLocaleTimeString();
 }
 
-function newUpdate () {
-mcping(settings.ip, settings.port, function(err, res) {
+function getServerStatus() {
+    mcping(settings.ip, settings.port, function(err, res) {
+        if (!(typeof err === 'undefined' || err === null)) {
+            client.user.setStatus('dnd');
+            serverStatus = 'Server offline';
+            client.user.setActivity(serverStatus, { type: 'PLAYING' });
+            getDate()
+            console.log((chalk.yellow('\[' + cleanDate + '\]:') + chalk.white(' Ping: ' +
+                'Error getting server status')));
+            console.error(err);
+            return;
+        }
+        if (typeof res.players.sample === 'undefined') { client.user.setStatus('idle') }
+        if (!(typeof res.players.sample === 'undefined')) { client.user.setStatus('online') }
+        serverStatus = res.players.online + ' / ' + res.players.max;
+        getDate()
+        client.user.setActivity(serverStatus, { type: 'PLAYING' }).then(presence => console.log(
+            chalk.cyan('\[' + cleanDate + '\]:') + chalk.white(' Ping: ' + serverStatus)
+        )).catch(console.error);
+    })
+}
 
-  if (!(typeof err === 'undefined' || err === null)) {
-      client.user.setStatus('dnd');
-      status2 = 'Server offline';
-      client.user.setActivity(status2, { type: 'PLAYING' });
-      getDate()
-      console.log((chalk.yellow('\[' + cleanDate + '\]:') + chalk.white(' Ping: ' + 'Error getting server status')));
-      console.error(err); return;}
-
- if (typeof res.players.sample === 'undefined') {client.user.setStatus('idle')}
- if (!(typeof res.players.sample === 'undefined')) {client.user.setStatus('online')}
-            
-          status2 = res.players.online + ' / ' + res.players.max;
-          getDate()
-          client.user.setActivity(status2, { type: 'PLAYING' })
-          .then(presence => console.log(chalk.cyan('\[' + cleanDate + '\]:') + chalk.white(' Ping: ' + status2)))
-          .catch(console.error);
-        })
-      }
-
-//On startup:
+//Startup:
 client.on("ready", () => {
-  console.log("Ready!");
-  newUpdate() //Ping the server once on startup, setting bot status
-  client.setInterval(newUpdate,settings.pingInterval);
+    console.log("Ready!");
+    getServerStatus()
+    client.setInterval(getServerStatus, settings.pingInterval);
 });
-
-
 
 //Command Handling
 client.on('message', message => {
-
-    if (!message.content.startsWith(settings.commandPrefix)) return; //If the message doesn't start with our prefix, ignore it
-    if (message.author.bot) return;{ //If the author of the message is a bot (including us)-- ignore it
-
-const args = message.content.slice(settings.commandPrefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
-
-//log the above
-console.log('Command \'' + message.content + '\' issued by ' + message.member.user.tag);
-console.log('Args: ' + args);
-
-
-//Help command handling
-if (command === "help" || command === "commands" || command === "list" | command === "bot"){
-
-      console.log('Issuing help message.');
-         message.channel.send({embed: {
-          color: 3447003,
-          title: 'Commands:',
-          description: '**/status** - The current status and playercount of your server \n**/crash** - Restart the bot'
-                            }}); return;
-
-       }
-
-     
-//Status command handling
-        else if (command === "status" || command === "server"){
-
-mcping(settings.ip, settings.port, function(err, res){
-    if (err) {
-        // Some kind of error
-        console.log(err);
-        message.channel.send('Error getting server status.'); return;
-    } else {
-        //console.log(res);
-        //console.log(res.description.extra);
-        //console.log(res.favicon)
-
-       
-        try {favicon = res.favicon.slice(22)
-              hasIcon = 'yes'
+    if (!message.content.startsWith(settings.commandPrefix)) return;
+    if (message.author.bot) return; {
+        const args = message.content.slice(settings.commandPrefix.length).trim().split(
+            / +/g);
+        const command = args.shift().toLowerCase();
+        //log the above
+        console.log('Command \'' + message.content + '\' issued by ' + message.member.user.tag);
+        console.log('Args: ' + args);
+        //Help command handling
+        if (command === "help" || command === "commands" || command === "list" | command ===
+            "bot") {
+            console.log('Issuing help message.');
+            message.channel.send({
+                embed: {
+                    color: 3447003,
+                    title: 'Commands:',
+                    description: '**/status** - The current status and player count of your server \n**/crash** - Restart the bot'
+                }
+            });
+            return;
         }
-
-        catch(error){
-          hasIcon = 'no'
-        }
-        
-
-        let onlinePlayers = [];
-
-
-if (typeof res.players.sample == 'undefined'){
-    status2 = '*No one is playing!*';
-} else {
-
-          for (var i = 0; i < res.players.sample.length; i++) {
-  
-  onlinePlayers.push(res.players.sample[i].name);
-};
-
-
-onlinePlayers = onlinePlayers.sort();
-onlinePlayers = onlinePlayers.join(', ');
-onlinePlayers = escape(onlinePlayers);
-         status2 = '**' + res.players.online + '/' + res.players.max + '**' + ' player(s) online.\n' + onlinePlayers;
-
-console.log('  ' + status2);
-        
-  };
-
-if (hasIcon === 'yes'){
-const buffer = Buffer.from(favicon, 'base64')
-const serverEmbedicon = new Discord.RichEmbed()
-    .attachFile({attachment:buffer, name: 'icon.png'})
-    .setTitle('Status for ' + settings.ip + ':')
-    .setColor(3447003)
-    .setDescription(status2)
-    .setThumbnail('attachment://icon.png')
-    .addField("Server version:", res.version.name)
-    message.channel.send(serverEmbedicon);
-}
-
-else if(hasIcon === 'no'){
-  const serverEmbedNoIcon = new Discord.RichEmbed()
-    .setTitle('Status for ' + settings.ip + ':')
-    .setColor(3447003)
-    .setDescription(status2)
-    .addField("Server version:", res.version.name)
-    message.channel.send(serverEmbedNoIcon);
-}
-
-    
+        //Status command handling
+        else if (command === "status" || command === "server" || command === "online") {
+            mcping(settings.ip, settings.port, function(err, res) {
+                if (err) {
+                    console.log(err);
+                    message.channel.send('Error getting server status.');
+                    return;
+                } else {
+                    try {
+                        favicon = res.favicon.slice(22)
+                        hasIcon = 'yes'
+                    } catch (error) {
+                        hasIcon = 'no'
+                    }
+                    let onlinePlayers = [];
+                    if (typeof res.players.sample == 'undefined') {
+                        serverStatus = '*No one is playing!*';
+                    } else {
+                        for (var i = 0; i < res.players.sample.length; i++) {
+                            onlinePlayers.push(res.players.sample[i].name);
+                        };
+                        onlinePlayers = onlinePlayers.sort();
+                        onlinePlayers = onlinePlayers.join(', ');
+                        onlinePlayers = escape(onlinePlayers);
+                        serverStatus = '**' + res.players.online + '/' + res.players.max +
+                            '**' + ' player(s) online.\n' + onlinePlayers;
+                        console.log('  ' + serverStatus);
+                    };
+                    if (hasIcon === 'yes') {
+                        const buffer = Buffer.from(favicon, 'base64')
+                        const serverEmbedicon = new Discord.RichEmbed().attachFile({ attachment: buffer,
+                            name: 'icon.png' }).setTitle('Status for ' +
+                            settings.ip + ':').setColor(3447003).setDescription(
+                            serverStatus).setThumbnail('attachment://icon.png').addField(
+                            "Server version:", res.version.name)
+                        message.channel.send(serverEmbedicon);
+                    } else if (hasIcon === 'no') {
+                        const serverEmbedNoIcon = new Discord.RichEmbed().setTitle(
+                                'Status for ' + settings.ip + ':').setColor(3447003)
+                            .setDescription(serverStatus).addField("Server version:",
+                                res.version.name)
+                        message.channel.send(serverEmbedNoIcon);
+                    }
+                }
+            }, 3000);
+            return;
+        } else if (command === "crash") { process.exit(0); } else return;
     }
-}, 3000);
-return;
-        }
-
-    
-      else if (command === "crash"){process.exit(0);}
-      else return;
-}})
-
+})
 client.login(settings.token);
